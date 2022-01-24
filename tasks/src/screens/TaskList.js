caa35195f2f11dbc6b4d2099c314525f1e22fe5b
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-24 15:21:52 -0300

    119-Componente lista de tarefas

---
+++ tasks/src/screens/TaskList.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react'
+import {  SafeAreaView,Text,StyleSheet,ImageBackground, View } from 'react-native'
+import TodayImage from '../../assets/imgs/today.jpg'
+import moment from 'moment'
+
+export default class TaskList extends Component{
+    render (){
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return (
+        <SafeAreaView style={styles.container}>
+        <ImageBackground source={TodayImage} style={styles.image}>
+            <View style={styles.titleBar}>
+                <Text>Hoje</Text>
+                <Text>{today}</Text>
+            </View>
+            </ImageBackground>
+        <View style={styles.taskList}>
+            <Text>React</Text>
+        </View>
+        </SafeAreaView>
+     )
+}
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+     },
+     image: {
+        flex: 3,
+      },
+     taskList:{
+         flex:7
+     },
+     titleBar :{
+         flex:1,
+         justifyContent: 'center',
+         alignItems:'center'
+     }
+    })
