c86be94a43a851718aecf86fa8e5a998a6628672
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-24 20:42:44 -0300

    140-Excluir Tarefa 02

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -17,8 +17,19 @@ export default props => {
             </TouchableOpacity>
         )
     }
+
+    const getLeftContent =  () =>{
+        return(
+            <View style={styles.left}>
+                <Icon style={styles.excludeIcon} name='trash' size={20} color='#FFF'></Icon>
+                <Text style={styles.excludeText}>Excluir</Text>
+            </View>
+        )
+    }
     return (
-        <Swipeable renderRightActions={getRightContent}>
+        <Swipeable 
+        renderRightActions={getRightContent}
+        renderLeftActions={getLeftContent}>
         <View style={styles.container}>
             <TouchableWithoutFeedback 
             onPress={()=>props.toggleTask(props.id)}>
@@ -58,7 +69,8 @@ const styles = StyleSheet.create({
         borderColor:'#AAA',
         borderBottomWidth:1,
         alignItems:'center',
-        paddingVertical:10
+        paddingVertical:10,
+        backgroundColor:'#FFF',
      },
      checkContainer:{
         width:'20%',
@@ -98,5 +110,20 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'flex-end',
         paddingHorizontal:20,
+     },
+     left:{
+         flex:1,
+        flexDirection:'row',
+        backgroundColor:'red',
+        alignItems:'center',
+     },
+     excludeText:{
+        fontFamily:commonStyles.fontFamily,
+        color:'#FFF',
+        fontSize:20,
+        margin:10,
+     },
+     excludeIcon:{
+         marginLeft:10,
      }
     })
