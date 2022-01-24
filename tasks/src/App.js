f7d83437b3e3dcbdc907ba814bfb92d3df3c7bd7
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-24 09:32:53 -0300

    Included Assets

---
+++ tasks/src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import { SafeAreaView,Text,StyleSheet } from 'react-native'
+
+export default class App extends Component{
+    render (){
+        return (
+        <SafeAreaView style={styles.container}>
+        <Text>App</Text>
+        </SafeAreaView>
+     )
+}
+}
+const styles = StyleSheet.create({
+    container:{
+
+     }})
